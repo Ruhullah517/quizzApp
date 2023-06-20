6759666b4c29b21645508c66de168a166f669262
Ruhullah517 <111290404+Ruhullah517@users.noreply.github.com>
2023-06-20 12:06:16 +0500

    Update messaging_init_in_sw.js

--- src/messaging_init_in_sw.js
+++ src/messaging_init_in_sw.js
@@ -4,13 +4,13 @@ import { getMessaging, getToken } from "firebase/messaging";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDc5HN475D4NVS6UisdkCxa7tzn5gBLbhI",
-    authDomain: "quizz-app-69ad9.firebaseapp.com",
-    projectId: "quizz-app-69ad9",
-    storageBucket: "quizz-app-69ad9.appspot.com",
-    messagingSenderId: "915358931941",
-    appId: "1:915358931941:web:4af0e6427980d13e44ed0c",
-    measurementId: "G-XQYYX88Y6F"
+    apiKey: `{process.env.APIKEY}`,
+    authDomain: `{process.env.AUTHDOMAIN}`,
+    projectId: `{process.env.PROJECTID}`,
+    storageBucket: `{process.env.STORAGEBUCKET}`,
+    messagingSenderId: `{process.env.MESSAGINGSENDERID}`,
+    appId: `{process.env.APPID}`,
+    measurementId: `{process.env.MEASUREMENTID}`
 };
 
 // Initialize Firebase
@@ -19,7 +19,7 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Cloud Messaging and get a reference to the service
 const messaging = getMessaging(app);
-getToken(messaging, { vapidKey: "BA89HiGbJ_O0mwGxgB5ZXRM-DncHY9k4S4cNMJmGCgVeKk2HeeoTfvjR3CYLd1SrHurB35QpehCDDND4MghsOPM" }).then((token) => {
+getToken(messaging, { vapidKey:`{process.env.VAPIDKEY}` }).then((token) => {
     if (token) {
         console.log("TOken", token);
     } else {
